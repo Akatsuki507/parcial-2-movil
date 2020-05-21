70ebd2cd203263b93133a9d25dfdb9463fed0164
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-21 17:31:04 -0500

    test de datos

--- app/src/main/java/com/example/appparcial2alvarado_deleon/login.java
+++ app/src/main/java/com/example/appparcial2alvarado_deleon/login.java
@@ -3,6 +3,7 @@ package com.example.appparcial2alvarado_deleon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class login extends base {
 
@@ -13,5 +14,13 @@ public class login extends base {
         // Metodos heredados de clase Base
         Load_or_inicializate_users();
         LoadAllData();
+
+        // Probando datos cargados
+        Log.e("TEST", "==============================================TEST====================================================");
+        Log.e("TEST", "CANTIDAD DE USUARIOS: " + String.valueOf(users.size()));
+        for (int counter = 0; counter < users.size(); counter++) {
+            Log.e("TEST", "| NOMBRE: " + users.get(counter).nombre + "| ROL: " + users.get(counter).rol);
+        }
+        Log.e("TEST", "==============================================TEST====================================================");
     }
 }
