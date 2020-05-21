848558b2b3b0950f28bcd6096a8f98a81951f32b
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-21 16:15:50 -0500

    clase base

---
+++ app/src/main/java/com/example/appparcial2alvarado_deleon/base.java
@@ -0,0 +1,9 @@
+package com.example.appparcial2alvarado_deleon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class base extends AppCompatActivity {
+
+}
