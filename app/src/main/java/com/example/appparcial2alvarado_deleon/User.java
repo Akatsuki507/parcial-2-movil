bcfed0e389f4a5eb1b304a4552fa8e3d88a59bcf
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-21 16:43:57 -0500

    modelo de usuario

---
+++ app/src/main/java/com/example/appparcial2alvarado_deleon/User.java
@@ -0,0 +1,17 @@
+package com.example.appparcial2alvarado_deleon;
+
+public class User {
+
+    public String cedula;
+    public String nombre;
+    public String pass;
+    public String rol;
+
+
+    public User(String cedula, String nombre, String pass, String rol) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.pass = pass;
+        this.rol = rol;
+    }
+}
