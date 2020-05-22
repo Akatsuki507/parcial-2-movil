1fe8e97d91ecffc9bf5dfe00a5debd70ea71940a
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-21 23:57:59 -0500

    GG

--- app/src/main/java/com/example/appparcial2alvarado_deleon/MainActivity.java
+++ app/src/main/java/com/example/appparcial2alvarado_deleon/MainActivity.java
@@ -19,53 +19,44 @@ public class MainActivity extends base {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(current_user.rol.equals("estudiante")) {
+            findViewById(R.id.rbtEstudiante).setVisibility(View.VISIBLE);
+            findViewById(R.id.rbtUTP).setVisibility(View.VISIBLE);
+            findViewById(R.id.rbtVirtual).setVisibility(View.VISIBLE);
+            findViewById(R.id.rbtProfesor).setVisibility(View.GONE);
+        }
+        else{
+            findViewById(R.id.rbtEstudiante).setVisibility(View.GONE);
+            findViewById(R.id.rbtUTP).setVisibility(View.VISIBLE);
+            findViewById(R.id.rbtVirtual).setVisibility(View.VISIBLE);
+            findViewById(R.id.rbtProfesor).setVisibility(View.VISIBLE);
+        }
+
         btnlink=findViewById((R.id.btnValidar));
                 btnlink.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Perfil=findViewById(R.id.Perfiles);
-                        if(current_user.rol.equals("estudiante")) {
-                            findViewById(R.id.rbtEstudiante).setVisibility(View.VISIBLE);
-                            if(Perfil.getCheckedRadioButtonId() == R.id.rbtEstudiante){
+                        if(Perfil.getCheckedRadioButtonId() == R.id.rbtEstudiante){
 
-                                Intent i =new Intent(getApplicationContext(),historial.class);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtProfesor){
-
-                                Intent i =new Intent(getApplicationContext(),historial.class);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtUTP){
-                                Uri uri = Uri.parse( "http://utp.ac.pa");
-                                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                                startActivity(intent);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtVirtual){
-                                Uri uri = Uri.parse("https://ecampus.utp.ac.pa/moodle/");
-                                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                                startActivity(intent);
-                            }
-|
+                            Intent i =new Intent(getApplicationContext(),historial.class);
                         }
-                        else{
-                            if(Perfil.getCheckedRadioButtonId() == R.id.rbtEstudiante){
+                        else if(Perfil.getCheckedRadioButtonId() == R.id.rbtProfesor){
 
-                                Intent i =new Intent(getApplicationContext(),historial.class);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtProfesor){
-
-                                Intent i =new Intent(getApplicationContext(),historial.class);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtUTP){
-                                Uri uri = Uri.parse( "http://utp.ac.pa");
-                                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                                startActivity(intent);
-                            }
-                            else if(Perfil.getCheckedRadioButtonId() == R.id.rbtVirtual){
-                                Uri uri = Uri.parse("https://ecampus.utp.ac.pa/moodle/");
-                                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                                startActivity(intent);
-                            }
+                            Intent i =new Intent(getApplicationContext(),historial.class);
                         }
+                        else if(Perfil.getCheckedRadioButtonId() == R.id.rbtUTP){
+                            Uri uri = Uri.parse( "http://utp.ac.pa");
+                            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                            startActivity(intent);
+                        }
+                        else if(Perfil.getCheckedRadioButtonId() == R.id.rbtVirtual){
+                            Uri uri = Uri.parse("https://ecampus.utp.ac.pa/moodle/");
+                            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                            startActivity(intent);
+                        }
+
 
                 }
     });
